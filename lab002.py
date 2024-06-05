0504f0130d1b8726c079a5d2b59fe4e7293d1340
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-05 20:16:44 +0530

    \n and \t code

---
+++ lab002.py
@@ -0,0 +1,12 @@
+print("deeksha", "gupta", sep='-', end="\t")
+print("Hello")
+
+print("deeksha", "gupta", sep='-', end="\n")
+print("Hello")
+
+print("deeksha", "gupta", sep='-', end="\n")
+print("Hello\tIndia")
+
+print("Hello\n\tIndia")
+print("Hello\n\tIndia\n\t\tBharat")
+
