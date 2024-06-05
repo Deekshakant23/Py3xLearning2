f8beacddd4280fdbcd00da6fd6c35e3b8747caa8
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-05 19:53:27 +0530

    new statement learn(end ,sep)

---
+++ lab001.py
@@ -0,0 +1,13 @@
+# print()
+# self - Concept in OOps which points to inself - ignore.
+# *args - Unlimited number of arguments * - string, int, float, boolean...
+# sep=' ' - How you want to seperate the arguments
+# end='\n' - in the end what you want to do
+# file=None - File IO
+
+
+print("hello wolrd", "india", "shade"
+      """hjyhguy""")
+print("hello wolrd", "india", end=' ')
+print("shade", "deeksha", sep='-')
+
