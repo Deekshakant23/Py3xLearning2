3393eded06c00b0a77118156a78706b7967995ba
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-06 19:48:20 +0530

    what kind of variable name we can take in python

---
+++ lab008.py
@@ -0,0 +1,24 @@
+
+age= 34
+numbers = 83493903
+emp_id = 67
+print(type(age))
+
+txt = 8.9
+print(type(txt))
+
+isfemale = True
+print(type(isfemale))
+
+isdeekshamarried = True
+print(type(isdeekshamarried))
+
+name = "deeksha"
+name2 ='gupta'
+name3 ="""deeksha 
+gupta
+delhi
+harpapur"""
+print(type(name))
+print(name2)
+print(name3)
