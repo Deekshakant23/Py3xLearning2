9a88dcbaed868f2653d0ead6676077606f774410
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-06 19:17:44 +0530

    what kind of variable name we can take in python

---
+++ lab006.py
@@ -0,0 +1,9 @@
+# Dynamically typed
+age=34
+print(type(age))
+
+age= "nine"
+print(type(age))
+
+age=8.3
+print(type(age))
---
+++ lab007.py
@@ -0,0 +1,33 @@
+
+# variable name should be start grom A-Z, a-z _
+name = "deeksha"
+print(type(name))
+
+# variable name should not start with number
+# 123 =345 - variable names
+
+
+# variable name should not be any keyword
+# keyword ? // Reserved word
+# 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield' [2]
+# and = 123
+
+# variable name should not be any special character
+# @ = 123
+# $ = 344
+_ = "deeksha"
+_name = "deeksha"
+print(_)
+print(type(_))
+print(type(_name))
+
+# $prampod = "pramod"
+# pramod$ = "Hello"
+
+# variable name should not be any space
+# first name = "deeksha"
+# Python love the snake case
+variable_name = "deeksha"
+
+long_var_name_is_created_here = "Hello"
+print(type(long_var_name_is_created_here))
