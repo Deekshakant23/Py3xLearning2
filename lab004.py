01f092df9e9071f1d5caf644a19c0af0927a8527
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-06 18:59:24 +0530

    variable and how to print variable by type function

--- lab004.py
+++ lab004.py
@@ -5,3 +5,4 @@ print(max(12,34,223,13))
 print(max(900,272, 279,8298))
 # Program to find the min two numbers
 print(min(34,325,34,35))
+# TypeError: '>' not supported between instances of 'str' and 'int'
---
+++ lab005.py
@@ -0,0 +1,45 @@
+# ✅ Dynamically typed
+# Dynamic Type - Type of Data that Python supports.
+age=23
+
+
+# variableName = variableValue
+# identifier = literal
+
+# Types which are supported in the Python
+
+# Integers - Positive and negative whole numbers.
+# 1, -1,123, 999, 100000, 96543210
+
+# Floating Points Numbers
+# 3.14, 5.3333333 , 18.00, 0.000786  . -0.4, -1.6
+pi=3.45
+
+# String
+# "pramod", "A", "hello world", "Hi, I am good person, You are a liar", "12345"
+name="Rohit"
+
+# Boolean
+# True, False
+# true, false ? boolean?
+is_good=True
+
+# How do I check the type of the variable?
+print(type(age))
+print(type(pi))
+print(type(name))
+print(type(is_good))
+
+# Python - Complex NUMBER - i iota -
+complex_number = 2 + 3j
+
+# Real - 2
+# Imaginary - 3
+print(type(complex_number.imag))
+print(type(complex_number.real))
+
+# Complex Data types in Python
+# List
+# Tuple
+# Dictionary
+# Set
