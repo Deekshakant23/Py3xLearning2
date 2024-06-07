7d340b72ba1b67edd3ceef590507f96985a85c10
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-07 19:26:45 +0530

    None data type and empty string

---
+++ ex07062024/lab018.py
@@ -0,0 +1,19 @@
+val = None
+print(val)
+# print(len(val))
+#val = val+1 # unsupported operand type(s) for +: 'NoneType' and 'int'
+# Data Type  - NoneType
+# Nothing
+# None is not a default value
+# None is not 0.
+# None is not an empty string.
+# None is not the same as False.
+
+name = "" # empty String - memory allocated
+print(name)
+print(len(name)) # 0 len
+name = None
+print(name)
+print(type(name)) # NoneType
+print(id(name))
+
