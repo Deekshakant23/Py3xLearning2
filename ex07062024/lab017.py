4707bf0123de1ab81e4b7b149c44f8c647122ee6
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-07 18:58:05 +0530

    array and how to dispalyed array lenth

---
+++ ex07062024/lab017.py
@@ -0,0 +1,8 @@
+name = "my husband is super hero"
+print(name)
+print(len(name))
+print(type(name))
+print(name[3]) #[] array value will deside the possition of  string ,out/p: h
+print(name[7])
+print(name[-8])
+print(name[-2])
