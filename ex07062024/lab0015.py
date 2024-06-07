39933d7b5c55d45430abf686742928def9adf2a4
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-07 17:46:59 +0530

    new function like formatting

--- ex07062024/lab0015.py
+++ ex07062024/lab0015.py
@@ -12,3 +12,11 @@ print(dir)
 first_name = 'harry'
 last_name = 'gupta'
 print(first_name +" " + last_name)
+print(first_name, last_name)
+# f -> formatting - it will replace the values of the variables
+#{} -> placeholders
+
+#without f
+print('Your Full name is {first_name} {last_name}')
+#with f
+print(f'Your Full name is {first_name} {last_name}')
