0f125f376e7ec967828f2b83298038950d279d4e
Deekshakant23 <deekshagupta237@gmail.com>
2024-06-07 06:47:09 +0530

    what kind of variable name we can take in python

--- lab010.py
+++ lab010.py
@@ -3,3 +3,4 @@ Firstname = input("Enter your first name: ")
 Lastname = input("Enter your last name: ")
 print("Hello " + Firstname + " " + Lastname)
 print("hello", "your name is ", Firstname, "and your last name is",Lastname )
+
